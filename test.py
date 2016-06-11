54fcd0ee0e0cdb67f2b4c86c4a2bdc1bee4c6a2a
Tim Specht <tim@tim-specht.de>
2016-06-11 21:04:53 +0200

    More stuff to recognize the text

--- test.py
+++ test.py
@@ -5,6 +5,8 @@ import nltk
 from math import ceil
 
 import pickle
+
+from fuzzywuzzy import fuzz
 from nltk.classify.scikitlearn import SklearnClassifier
 from nltk import word_tokenize, stem, tokenize, metrics
 from sklearn.linear_model import LogisticRegression, SGDClassifier
@@ -35,12 +37,14 @@ features = {
         "Tell me the color of this object",
     ],
     ClASS_WHERE_IS: [
-        "Where is",
-        "Can you find",
-        "Please find",
-        "Is there a",
+        "Where is in the picture",
+        "Can you find in the picture",
+        "Where is in the image",
+        "Can you find in the image",
+        "Please find in the picture",
+        "Is there a here",
         "Is there",
-        "Tell me where",
+        "Tell me where to find",
         "Help me find"
     ]
 }
@@ -59,38 +63,46 @@ labeled_sentences += [
     ("What is the weather going to be tomorrow", CLASS_UNKNOWN),
     ("Birds are animals", CLASS_UNKNOWN),
     ("Plants are not", CLASS_UNKNOWN),
-    ("Awesome is what awesome is", CLASS_UNKNOWN)
+    ("Awesome is what awesome is", CLASS_UNKNOWN),
+    ("Coloring  is not an option", CLASS_UNKNOWN),
+    ("We have to admit that color looks weird", CLASS_UNKNOWN),
+    ("No one knows where he is", CLASS_UNKNOWN)
 ]
 
 stemmer = stem.PorterStemmer()
 
-#
-# def normalize(s):
-#     words = tokenize.wordpunct_tokenize(s.lower().strip())
-#     return ' '.join([stemmer.stem(w) for w in words])
-#
-#
-# def fuzzy_match(s1, s2, max_dist=3):
-#     return metrics.edit_distance(normalize(s1), normalize(s2)) <= max_dist
-#
-#
-# def feature_set_for_input(input):
-#     tokens = word_tokenize(input)
-#
-#     features = ["what", "color", "find", "see"]
-#     result = {}
-#     for feature in features:
-#         result[feature] = sum([1 if fuzzy_match(t, feature) else 0 for t in tokens])
-#
-#     return result
-#
-# shuffle(labeled_sentences)
-#
-# feature_sets = [(feature_set_for_input(sentence), sentence_class) for (sentence, sentence_class) in labeled_sentences]
-# cutting_point = ceil(len(feature_sets) * 0.7)
-# train_set, test_set = feature_sets[cutting_point:], feature_sets[:cutting_point]
-#
-# classifiers = [MultinomialNB, BernoulliNB, LogisticRegression, SGDClassifier, SVC, LinearSVC, NuSVC]
+
+def normalize(s):
+    words = tokenize.wordpunct_tokenize(s.lower().strip())
+    return ' '.join([stemmer.stem(w) for w in words])
+
+
+def fuzzy_match(s1, s2):
+    return fuzz.ratio(normalize(s1), normalize(s2)) >= 80
+
+
+def feature_set_for_input(input):
+    tokens = word_tokenize(input)
+
+    features = ["what .* color", "find", "see"]
+    result = {}
+    total_sum = 0
+    for feature in features:
+        result[feature] = sum([1 if fuzzy_match(t, feature) else 0 for t in tokens])
+        total_sum += result[feature]
+
+    # Add another feature that the sentence is not super long and only contains one item
+    # result["total"] = total_sum/len(tokens)
+
+    return result
+
+shuffle(labeled_sentences)
+
+feature_sets = [(feature_set_for_input(sentence), sentence_class) for (sentence, sentence_class) in labeled_sentences]
+cutting_point = ceil(len(feature_sets) * 0.7)
+train_set, test_set = feature_sets[cutting_point:], feature_sets[:cutting_point]
+
+classifiers = [MultinomialNB, BernoulliNB, LogisticRegression, SGDClassifier, SVC, LinearSVC, NuSVC]
 
 
 class RobotEyesNaturalLanguageClassifierV1(NaturalLanguageClassifierV1):
@@ -106,7 +118,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Train a NLTK model to categorize input questions.')
     parser.add_argument('mode', type=str, help='The mode this script should run in')
     args = parser.parse_args()
-    #
+
     # if args.mode == "train":
     #     best_score = 0.0
     #     best_classifier = None
@@ -131,7 +143,8 @@ if __name__ == "__main__":
     #     while True:
     #         print(classifier.classify(feature_set_for_input(input("Give me some data:"))))
 
-    natural_language_classifier = RobotEyesNaturalLanguageClassifierV1(api_key='6f9cdb76ca40cc55c4a87ef22bce7a4a3f4aea47')
+    natural_language_classifier = NaturalLanguageClassifierV1(username="d08848f4-f212-449c-8a70-e6918e8df154",
+                                                              password="6wt2ixCv2Vkr")
 
     classifiers = natural_language_classifier.list()
     print(json.dumps(classifiers, indent=2))
@@ -139,8 +152,8 @@ if __name__ == "__main__":
     # create a classifier
     if args.mode == "train":
         with open('test.csv', 'rb') as training_data:
-            print(json.dumps(natural_language_classifier.create(training_data=training_data, name='weather'), indent=2))
+            print(json.dumps(natural_language_classifier.create(training_data=training_data, name='question'), indent=2))
 
     else:
-        classes = natural_language_classifier.classify('3a84d1x62-nlc-18096', input("Give me some data"))
+        classes = natural_language_classifier.classify('2373f5x67-nlc-2355', input("Give me some data"))
         print(json.dumps(classes, indent=2))
